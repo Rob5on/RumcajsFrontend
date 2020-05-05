f61c8999e286d1cc906961d75a5c650d8dbadcc0
Vekar98 <Vekar98@users.noreply.github.com>
2020-05-05 13:37:37 +0200

    RJS-68

---
+++ Decoracion.java
@@ -0,0 +1,36 @@
+package org.mensajes.ventanas.estilo;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class Decoracion {
+
+	// Default decoration for the JTextField
+	public static JTextField setBasicEstiloTXT(JTextField campo) {
+
+		campo.setBackground(Paleta.COLOR_SECUNDARIO);
+		campo.setBorder(new RoundedBorder(8, Paleta.COLOR_CLARO));
+		campo.setForeground(Color.white);
+		campo.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+		campo.setPreferredSize(new Dimension(0, 32));
+
+		return campo;
+	}
+
+	// Default decoration for the JButton
+	public static JButton setAceptBTN(JButton btn) {
+
+		btn.setBackground(Paleta.ACEPTAR);
+		btn.setBorder(new RoundedBorder(8, Paleta.ACEPTAR));
+		btn.setForeground(Paleta.ACEPTAR_2);
+		btn.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+		btn.setPreferredSize(new Dimension(0, 40));
+
+		return btn;
+	}
+
+}
